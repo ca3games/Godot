14666d9775aa5ba09e5be6578fd3b7bdab81e800
ca3games <ca3games@gmail.com>
2018-04-02 00:01:37 -0500

    Superion 3D setup

---
+++ Superion 3D/Code/Ball.gd
@@ -0,0 +1,44 @@
+extends RigidBody
+var left = false
+var top =  true
+var vel = 5
+var speed = Vector2(0,0)
+
+func _ready():
+	set_translation(Vector3(get_translation().x, get_translation().y, Variables.z))
+	connect("body_enter", self, "_bodyenter")
+	get_node("AnimationPlayer").play("IDLE")
+	speed = Vector2(vel, vel)
+	set_fixed_process(true)
+	
+func _bodyenter(body):
+	if body.get_name() == "Right":
+		left = true
+	if body.get_name() == "Left":
+		left = false
+	if body.get_name() == "Top":
+		top = false
+	if body.get_name() == "Bottom":
+		top = true
+	if body.is_in_group("Enemy"):
+		left = !left
+	if body.get_name() == "Player":
+		top = !top
+		left = !body.right
+	set_angular_velocity(Vector3(0,0,0))
+	get_node("AnimationPlayer").play("Bounce")
+
+func _stopbounce():
+	get_node("AnimationPlayer").play("IDLE")
+
+func _fixed_process(delta):
+	if left:
+		speed.x = -vel
+	else:
+		speed.x = vel
+	if top:
+		speed.y = vel
+	else:
+		speed.y = -vel
+	
+	set_linear_velocity(Vector3 (speed.x, speed.y, 0))
---
+++ Superion 3D/Code/Enemy.gd
@@ -0,0 +1,37 @@
+extends RigidBody
+
+var timer = -99
+var left
+var vel
+
+func _ready():
+	get_node("AnimationPlayer").play("IDLE")
+	connect("body_enter", self, "_bounce")
+	left = false
+	vel = 2
+	set_rotation(Vector3(0, 64.4, 0))
+	set_translation(Vector3(get_translation().x, get_translation().y, Variables.z))
+	set_fixed_process(true)
+	
+func _bounce(body):
+	get_node("AnimationPlayer").play("Bounce")
+	if body.get_name() == "Ball":
+		timer = 5
+	if body.get_name() == "Left":
+		left = false
+	if body.get_name() == "Right":
+		left = true
+	if body.is_in_group("Enemy"):
+		left = !left
+	
+func _fixed_process(delta):
+	if timer > 0:
+		timer -= delta * 2
+	else:
+		if left:
+			set_linear_velocity(Vector3(-vel, 0, 0))
+		else:
+			set_linear_velocity(Vector3(vel, 0, 0))
+	
+func _stop():
+	get_node("AnimationPlayer").play("IDLE")
