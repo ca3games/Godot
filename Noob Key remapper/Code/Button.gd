334d7ffc0d9928a11ec160fb372db80f77d4d336
ca3games <ca3games@gmail.com>
2021-12-20 20:29:16 -0500

    noob remap MIDI input

--- Noob Key remapper/Code/Button.gd
+++ Noob Key remapper/Code/Button.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+export (NodePath) var PythonCodePath
+onready var PythonCode = get_node(PythonCodePath)
+export(NodePath) var OutPutListPath
+onready var OutPutList = get_node(OutPutListPath)
 
 func _ready():
 	yield(get_tree(), "idle_frame")
-	var ports = $"../PythonCode".GetPortsSize()
+	var ports = PythonCode.GetPortsSize()
 	for i in ports:
-		$"../OutputPorts".add_item($"../PythonCode".GetPortName(i), i)
+		OutPutList.add_item(PythonCode.GetPortName(i), i)
--- Noob Key remapper/Code/OutputPorts.gd
+++ Noob Key remapper/Code/OutputPorts.gd
@@ -2,6 +2,11 @@ extends OptionButton
 
 var id = 0
 
+export (NodePath) var PythonCodePath
+onready var PythonCode = get_node(PythonCodePath)
+export(NodePath) var OutPutLabelPath
+onready var OutPutLabel = get_node(OutPutLabelPath)
+
 func _ready():
 	yield(get_tree(), "idle_frame")
 	yield(get_tree(), "idle_frame")
@@ -16,5 +21,5 @@ func _on_SetOutputPort_button_up():
 	SetPort()
 	
 func SetPort():
-	$"../PythonCode".ChangePort(id)
-	$"../CurrentPortLabel".text = str(id) + " : " + $"../PythonCode".GetPortName(id)
+	PythonCode.ChangePort(id)
+	OutPutLabel.text = str(id) + " : " + PythonCode.GetPortName(id)
